5b766c7758bee0a5a0032d2735b310099370474a
tremebon <baladium@hotmail.it>
2014-11-20 16:43:09 +0100

    set up for addressing the camera issue
    added a -Mirror test level- to the "official" unity project.
    you're welcome to try and get this camera mess sorted, i have no idea
    how to get the mirror world camera to display onto a plane w/out using
    render2texture (unity pro).

---
+++ Game_things/Assets/Mirror test level/Scripts/FirstPersonController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class FirstPersonController : MonoBehaviour {
+
+	public float movementSpeed = 5.0f;
+	public float mouseSensitivity = 3.0f;
+	public float verticalRange = 60.0f;
+	public float jumpSpeed = 1.5f;
+	float verticalRotation = 0;
+	float verticalVelocity = 0;
+	CharacterController characterController;
+
+	// Use this for initialization
+	void Start () {
+		Screen.lockCursor = true;
+		characterController = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float sideRotation = mouseSensitivity * Input.GetAxis ("Mouse X");
+		transform.Rotate (0, sideRotation, 0);
+
+		verticalRotation -= mouseSensitivity * Input.GetAxis ("Mouse Y");
+		verticalRotation = Mathf.Clamp (verticalRotation, -verticalRange, verticalRange);
+		Camera.main.transform.localRotation =  Quaternion.Euler(verticalRotation, 0, 0);
+
+		float forwardSpeed = movementSpeed * Input.GetAxis ("Vertical");
+		float sideSpeed = movementSpeed * Input.GetAxis ("Horizontal");
+
+		verticalVelocity = Input.GetButtonDown ("Jump") && characterController.isGrounded ?
+			jumpSpeed :
+			verticalVelocity + Physics.gravity.y * Time.deltaTime;
+
+		Vector3 speed = new Vector3 (sideSpeed, verticalVelocity, forwardSpeed);
+		speed = transform.rotation * speed;
+
+		characterController.Move (speed * Time.deltaTime);
+	}
+}
---
+++ Game_things/Assets/Mirror test level/Scripts/MirroredFollow.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+ 
+var objectBeforeMirror : GameObject;
+var mirrorPlane : GameObject;
+ 
+function Update () 
+{
+   if (null != mirrorPlane) 
+   {
+      if (null != renderer)
+		 renderer.sharedMaterial.SetMatrix("_WorldToMirror", 
+         	mirrorPlane.renderer.worldToLocalMatrix);
+      if (null != objectBeforeMirror) 
+      {
+         transform.position = objectBeforeMirror.transform.position;
+         transform.rotation = objectBeforeMirror.transform.rotation;
+         transform.localScale = 
+            -objectBeforeMirror.transform.localScale; 
+         transform.RotateAround(objectBeforeMirror.transform.position, 
+            mirrorPlane.transform.TransformDirection(
+            Vector3(0.0, 1.0, 0.0)), 180.0);
+ 
+         var positionInMirrorSpace : Vector3 = 
+            mirrorPlane.transform.InverseTransformPoint(
+            objectBeforeMirror.transform.position);
+         positionInMirrorSpace.y = -positionInMirrorSpace.y;
+         transform.position = mirrorPlane.transform.TransformPoint(
+            positionInMirrorSpace);
+      }
+   }
+}
