935357acaa2bab9508fc4255b8e64207fb788a6b
Paulo Nesello <kruger_paulo@hotmail.com>
2014-12-18 18:02:06 +0100

    Conveyor-belt/Wind PREFAB
    Added a prefab with script for all our wind and conveyor belt needs!

---
+++ Game_things/Assets/Windy_regions/windyScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class windyScript : MonoBehaviour {
+	
+	public Vector3 speed;
+	
+	public void OnTriggerStay(Collider col) {
+		CharacterController ctrl = col.gameObject.GetComponent(typeof(CharacterController)) as CharacterController;
+		if (ctrl) {
+			ctrl.SimpleMove(speed);
+		}
+	}
+}
